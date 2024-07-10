ab3977b3bf4ad39384735d77819a9d4b5ba20cfc
shogek <benas.bagdanovas@gmail.com>
2024-07-10 09:37:42 +0300

    Complete `SetupCornerstoneComponent`

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { useRef } from 'react'
 import { FileInputComponent } from './components/file-input-component/file-input-component'
 import { MedicalImageCanvas } from './components/medical-image-canvas/medical-image-canvas'
 import { SetupCornerstoneComponent } from './components/setup-cornerstone-component/setup-cornerstone-component'
-import './app.css'
 import { ErrorMessage } from './components/error-message/error-message'
 import { CSToolsButtons } from './components/cs-tools-buttons/cs-tools-buttons'
 import { CSAnnotations } from './components/cs-annotations/cs-annotations'
 import { ReportSummaryComponent } from './components/report-summary-component/report-summary-component'
+import './app.css'
 
 function App() {
    const imageRef = useRef<HTMLCanvasElement | null>(null)
--- src/components/setup-cornerstone-component/setup-cornerstone-component.tsx
+++ src/components/setup-cornerstone-component/setup-cornerstone-component.tsx
@@ -1,4 +1,32 @@
+import { useRef } from 'react'
+import * as cornerstone from 'cornerstone-core'
+import * as cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader'
+import * as cornerstoneTools from 'cornerstone-tools'
+import * as cornerstoneMath from 'cornerstone-math'
+import * as dicomParser from 'dicom-parser'
+import Hammer from 'hammerjs'
+
+const setupCornerstone = (): void => {
+   cornerstoneTools.external.cornerstone = cornerstone
+   cornerstoneTools.external.cornerstoneMath = cornerstoneMath
+   cornerstoneTools.external.Hammer = Hammer
+   cornerstoneWADOImageLoader.external.cornerstone = cornerstone
+   cornerstoneWADOImageLoader.external.dicomParser = dicomParser
+
+   cornerstoneTools.init({
+      globalToolSyncEnabled: true,
+      showSVGCursors: true,
+   })
+}
+
 export function SetupCornerstoneComponent() {
-   // TODO: setup cornerstone once
+   const isInitRef = useRef(false)
+
+   // TODO: Double check that this gets called once even in StrictMode?
+   if (!isInitRef.current) {
+      isInitRef.current = true
+      setupCornerstone()
+   }
+
    return <></>
 }
